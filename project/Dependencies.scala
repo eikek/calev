371dabb7988a7a0363edabcf5e1009a49623c907
Scala Steward <me@scala-steward.org>
2021-03-30 17:03:57 +0200

    Update fs2-core, fs2-io to 3.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val circeVersion = "0.13.0"
   val doobieVersion = "0.12.1"
-  val fs2Version = "2.5.3"
+  val fs2Version = "3.0.1"
   val h2Version = "1.4.200"
   val log4sVersion = "1.8.2"
   val logbackVersion = "1.2.3"
