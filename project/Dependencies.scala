83ac4074750ab67ef1a1a14942fc8812aaa1e85e
Scala Steward <me@scala-steward.org>
2021-05-24 00:14:47 +0200

    Update doobie-core to 0.13.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val circeVersion    = "0.13.0"
-  val doobieVersion   = "0.13.3"
+  val doobieVersion   = "0.13.4"
   val fs2Version      = "2.5.6"
   val h2Version       = "1.4.200"
   val log4sVersion    = "1.8.2"
