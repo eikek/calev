ee415f3d56b46d15d87653469497db442eb4a64b
pawelkaczor <newion@o2.pl>
2021-06-28 07:50:33 +0200

    fix CalEventSerializer

--- modules/jackson/src/main/scala/com/github/eikek/calev/jackson/CalEventSerializer.scala
+++ modules/jackson/src/main/scala/com/github/eikek/calev/jackson/CalEventSerializer.scala
@@ -1,31 +1,11 @@
 package com.github.eikek.calev.jackson
 
-import java.io.IOException
-
-import com.fasterxml.jackson.core.JsonGenerator
-import com.fasterxml.jackson.core.JsonToken
-import com.fasterxml.jackson.databind.SerializerProvider
-import com.fasterxml.jackson.databind.jsontype.TypeSerializer
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializerBase
 import com.github.eikek.calev.CalEvent
 
 @SerialVersionUID(1L)
-class CalEventSerializer() extends ToStringSerializerBase(classOf[CalEvent]) {
-  @throws[IOException]
-  override def serializeWithType(
-      value: Any,
-      g: JsonGenerator,
-      provider: SerializerProvider,
-      typeSer: TypeSerializer
-  ): Unit = {
-    val typeIdDef = typeSer.writeTypePrefix(
-      g,
-      typeSer.typeId(value, classOf[CalEvent], JsonToken.VALUE_STRING)
-    )
-    serialize(value, g, provider)
-    typeSer.writeTypeSuffix(g, typeIdDef)
-    ()
-  }
+class CalEventSerializer extends ToStringSerializerBase(classOf[CalEvent]) {
+
   override def valueToString(value: Any): String =
     value.asInstanceOf[CalEvent].asString
 }
