945b57eaec743a0af38c1e4dbce8ee702c43c517
Scala Steward <me@scala-steward.org>
2021-06-30 00:12:49 +0200

    Update fs2-core, fs2-io to 2.5.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val akkaVersion      = "2.6.15"
   val circeVersion     = "0.14.1"
   val doobieVersion    = "0.13.4"
-  val fs2Version       = "2.5.6"
+  val fs2Version       = "2.5.7"
   val h2Version        = "1.4.200"
   val jacksonVersion   = "2.12.3"
   val log4sVersion     = "1.8.2"
