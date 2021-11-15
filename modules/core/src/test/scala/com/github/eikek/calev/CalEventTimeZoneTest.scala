3591423d9455203abb32552dddb26d4da3f98722
pawelkaczor <newion@o2.pl>
2021-11-15 17:44:54 +0100

    test nextElapse with time zone

---
+++ modules/core/src/test/scala/com/github/eikek/calev/CalEventTimeZoneTest.scala
@@ -0,0 +1,26 @@
+package com.github.eikek.calev
+
+import com.github.eikek.calev.Dsl._
+import munit._
+
+import java.time.{ZoneId, ZonedDateTime}
+
+class CalEventTimeZoneTest extends FunSuite {
+
+  test("next elapse with time zone") {
+    val refInUtc: ZonedDateTime =
+      ZonedDateTime.of(2021, 11, 15, 11, 15, 0, 0, ZoneId.of("CET"))
+        .withZoneSameInstant(ZoneId.of("UTC"))
+
+    assertEquals(refInUtc.getHour, 10)
+    assertEquals(refInUtc.getZone, ZoneId.of("UTC"))
+
+    val ce = CalEvent(Mon ~ Tue, DateEvent.All, time(11.c, 23.c, 0.c))
+
+    val result: Option[ZonedDateTime] = ce.nextElapse(refInUtc)
+
+    assertEquals(result.map(_.getZone), Some(ZoneId.of("UTC")))
+    assertEquals(result, Some(refInUtc.withMinute(23)))
+  }
+
+}
