a8170b5727cb7f227012459af15488c87d101c8e
pawelkaczor <newion@o2.pl>
2021-11-15 17:50:37 +0100

    fix formatting

--- modules/core/src/test/scala/com/github/eikek/calev/CalEventTimeZoneTest.scala
+++ modules/core/src/test/scala/com/github/eikek/calev/CalEventTimeZoneTest.scala
@@ -9,7 +9,8 @@ class CalEventTimeZoneTest extends FunSuite {
 
   test("next elapse with time zone") {
     val refInUtc: ZonedDateTime =
-      ZonedDateTime.of(2021, 11, 15, 11, 15, 0, 0, ZoneId.of("CET"))
+      ZonedDateTime
+        .of(2021, 11, 15, 11, 15, 0, 0, ZoneId.of("CET"))
         .withZoneSameInstant(ZoneId.of("UTC"))
 
     assertEquals(refInUtc.getHour, 10)
