b1cecc67e2dfbbbe55b172359c4b89dfc014cb7a
eikek-scala-steward <eikek-scala-steward@users.noreply.github.comb>
2022-07-31 06:12:02 +0000

    Update scalatest to 3.2.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   val log4sVersion = "1.8.2"
   val logbackVersion = "1.2.11"
   val munitVersion = "0.7.29"
-  val scalaTestVersion = "3.2.12"
+  val scalaTestVersion = "3.2.13"
   val organizeImportsVersion = "0.6.0"
 
   val organizeImports = Seq(
