2e8d84920e4323916a375bd816e30be654c107ae
Scala Steward <scala_steward@virtuslab.com>
2022-09-17 16:20:42 +0000

    Update logback-classic to 1.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val h2Version = "2.1.214"
   val jacksonVersion = "2.13.4"
   val log4sVersion = "1.8.2"
-  val logbackVersion = "1.4.0"
+  val logbackVersion = "1.4.1"
   val munitVersion = "0.7.29"
   val scalaTestVersion = "3.2.13"
   val organizeImportsVersion = "0.6.0"
