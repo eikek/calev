4a5068c3be03e6fb5442d8a67a136a961f74f089
eikek-scala-steward <eikek-scala-steward@users.noreply.github.comb>
2023-01-30 06:18:40 +0000

    Update jackson-module-scala to 2.14.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val doobieVersion = "1.0.0-RC2"
   val fs2Version = "3.5.0"
   val h2Version = "2.1.214"
-  val jacksonVersion = "2.14.1"
+  val jacksonVersion = "2.14.2"
   val log4sVersion = "1.8.2"
   val logbackVersion = "1.4.5"
   val munitVersion = "0.7.29"
