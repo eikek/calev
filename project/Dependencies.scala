8c0fda0b85e2a029716358406d0746a946a84353
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2023-11-07 06:28:19 +0000

    Update fs2-core, fs2-io to 3.9.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val akkaVersion = "2.6.20"
   val circeVersion = "0.14.6"
   val doobieVersion = "1.0.0-RC4"
-  val fs2Version = "3.9.2"
+  val fs2Version = "3.9.3"
   val h2Version = "2.2.224"
   val jacksonVersion = "2.15.3"
   val log4sVersion = "1.8.2"
