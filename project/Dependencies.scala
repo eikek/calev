beef3451d0ee595c9dbc13bcc8206a64df5fa1c1
Scala Steward <scala_steward@virtuslab.com>
2024-05-23 15:28:40 +0000

    Update munit, munit-scalacheck to 1.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   val jacksonVersion = "2.17.1"
   val log4sVersion = "1.8.2"
   val logbackVersion = "1.5.6"
-  val munitVersion = "0.7.29"
+  val munitVersion = "1.0.0"
   val scalaTestVersion = "3.2.18"
 
   val scalaTest = Seq(
