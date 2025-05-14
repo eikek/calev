7d693e7750471da65da7b3a0d56660714313ed70
Scala Steward <scala_steward@virtuslab.com>
2025-05-14 15:28:55 +0000

    Update jackson-databind to 2.19.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val doobieVersion = "1.0.0-RC8"
   val fs2Version = "3.11.0"
   val h2Version = "2.3.232"
-  val jacksonVersion = "2.18.4"
+  val jacksonVersion = "2.19.0"
   val log4sVersion = "1.8.2"
   val logbackVersion = "1.5.17"
   val munitVersion = "1.1.0"
