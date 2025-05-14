6b372a631d494dc43936963fa2e85f61fbcf1272
Scala Steward <scala_steward@virtuslab.com>
2025-05-14 15:29:45 +0000

    Update doobie-core to 1.0.0-RC9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val akkaVersion = "2.6.20"
   val circeVersion = "0.14.13"
-  val doobieVersion = "1.0.0-RC8"
+  val doobieVersion = "1.0.0-RC9"
   val fs2Version = "3.11.0"
   val h2Version = "2.3.232"
   val jacksonVersion = "2.18.4"
