ddd23e5c444e11ca68048786f0cc7ed84b4baa42
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2025-10-01 06:21:11 +0000

    Update circe-core, circe-generic, ... to 0.14.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val akkaVersion = "2.6.20"
-  val circeVersion = "0.14.14"
+  val circeVersion = "0.14.15"
   val doobieVersion = "1.0.0-RC10"
   val fs2Version = "3.12.2"
   val h2Version = "2.4.240"
